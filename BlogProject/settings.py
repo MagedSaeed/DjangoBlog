b4f6797f65261bd6b07f73dad93cd4f29525f8fd
MagedSaeed <mageedsaeed1@gmail.com>
2019-09-04 19:26:41 +0300

    add profile pic

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -122,3 +122,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'Blog:blog-home'
 
 LOGIN_URL = 'blog-login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
--- BlogProject/urls.py
+++ BlogProject/urls.py
@@ -18,11 +18,16 @@ from django.urls import path, include
 from Users import views as user_view
 from django.contrib.auth import views as auth_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Blog.urls')),
     path('register/', user_view.register, name="blog-register"),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='blog-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='blog-logout'),
-    path('profile/',user_view.profile,name='blog-profile')
+    path('profile/', user_view.profile, name='blog-profile'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
