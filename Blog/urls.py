bcdf49bad7eea67d4899ba9d46cd39c8a1f75f8a
MagedSaeed <mageedsaeed1@gmail.com>
2019-09-09 12:58:53 +0300

    Create Post added

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from django.contrib import admin
+from .views import PostListView, PostDetailView, PostCreateView
 
 from . import views
 app_name = 'Blog'
 urlpatterns = [
-    path('', views.home, name="blog-home"),
-    path('about/', views.about, name="blog-about")
+    # path('', views.home, name="blog-home"),
+    path('', PostListView.as_view(), name="blog-home"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('post/new/', PostCreateView.as_view(), name="post-create"),
+    path('about/', views.about, name="blog-about"),
+
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
 
 # Create your views here.
 
@@ -14,3 +16,24 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html')
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
